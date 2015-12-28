a42f32f8f578e8a2120a1870d3a29c001004207f
Swapnanil Saha <swapnanilsaha26@gmail.com>
2015-12-29 00:01:31 +0530

    cl ready

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Desciption
 Sends bulk emails to email addresses read from excel file(s)
 
 
+Usage
+-----
+node main.js
+There must be a file named 'test.xlsx' in the same directory as main.js
+
--- main.js
+++ main.js
@@ -1,11 +1,39 @@
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill('2t6q7YkVFC4KvKdlkC6sRg');
+var message = {
+    "text": "yo babes",
+    "subject": "the beatles",
+    "from_email": "fly@confluenceedu.com",
+    "from_name": "__PRO__",
+    "headers": {
+        "Reply-To": "fly@confluenceedu.com"
+    }
+};
+
 if(typeof require !== 'undefined') XLSX = require('xlsx');
 var workbook = XLSX.readFile('test.xlsx');
 var sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(function(y) { /* iterate through sheets */
+sheet_name_list.forEach(function(y) {
   var worksheet = workbook.Sheets[y];
   for (z in worksheet) {
-    /* all keys that do not begin with "!" correspond to cell addresses */
     if(z[0] === '!') continue;
-    console.log(JSON.stringify(worksheet[z].v));
+    message['to'] = [{
+            "email": worksheet[z].v,
+            "name": "Cool guys and gals",
+            "type": "to"
+        }];
+    if(validateEmail(worksheet[z].v)) {
+	    mandrill_client.messages.send({"message": message}, function(result) {
+		    console.log(result);
+		}, function(e) {
+		    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+		});
+	}
+    console.log("Email to: " + worksheet[z].v);
   }
 });
