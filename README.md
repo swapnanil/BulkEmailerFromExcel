fce407c3819bb4ec6d37b1e223abf52f5408a24b
Swapnanil Saha <swapnanilsaha26@gmail.com>
2015-12-29 00:14:51 +0530

    content read from file

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Sends bulk emails to email addresses read from excel file(s)
 
 Usage
 -----
-node main.js
-There must be a file named 'test.xlsx' in the same directory as main.js
+node main.js  
+There must be a file named 'test.xlsx' in the same directory as main.js  
+There must be a file named 'content.html' in the same directory as main.js  
 
--- main.js
+++ main.js
@@ -5,35 +5,44 @@ function validateEmail(email) {
 
 var mandrill = require('mandrill-api/mandrill');
 var mandrill_client = new mandrill.Mandrill('2t6q7YkVFC4KvKdlkC6sRg');
-var message = {
-    "text": "yo babes",
-    "subject": "the beatles",
-    "from_email": "fly@confluenceedu.com",
-    "from_name": "__PRO__",
-    "headers": {
-        "Reply-To": "fly@confluenceedu.com"
-    }
-};
 
-if(typeof require !== 'undefined') XLSX = require('xlsx');
-var workbook = XLSX.readFile('test.xlsx');
-var sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(function(y) {
-  var worksheet = workbook.Sheets[y];
-  for (z in worksheet) {
-    if(z[0] === '!') continue;
-    message['to'] = [{
-            "email": worksheet[z].v,
-            "name": "Cool guys and gals",
-            "type": "to"
-        }];
-    if(validateEmail(worksheet[z].v)) {
-	    mandrill_client.messages.send({"message": message}, function(result) {
-		    console.log(result);
-		}, function(e) {
-		    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-		});
-	}
-    console.log("Email to: " + worksheet[z].v);
-  }
+var message = {};
+fs = require('fs')
+fs.readFile('content.html', 'utf8', function (err, data) {
+    message = {
+        "text" : data,
+        "html" : data,
+        "subject": "the beatles",
+        "from_email": "fly@confluenceedu.com",
+        "from_name": "__PRO__",
+        "headers": {
+            "Reply-To": "fly@confluenceedu.com"
+        }
+    };
+    readAndSend();
 });
+
+function readAndSend() {
+    if(typeof require !== 'undefined') XLSX = require('xlsx');
+    var workbook = XLSX.readFile('test.xlsx');
+    var sheet_name_list = workbook.SheetNames;
+    sheet_name_list.forEach(function(y) {
+      var worksheet = workbook.Sheets[y];
+      for (z in worksheet) {
+        if(z[0] === '!') continue;
+        message['to'] = [{
+                "email": worksheet[z].v,
+                "name": "Cool guys and gals",
+                "type": "to"
+            }];
+        if(validateEmail(worksheet[z].v)) {
+    	    mandrill_client.messages.send({"message": message}, function(result) {
+    		    console.log(result);
+    		}, function(e) {
+    		    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+    		});
+    	}
+        console.log("Email to: " + worksheet[z].v);
+      }
+    });
+}
